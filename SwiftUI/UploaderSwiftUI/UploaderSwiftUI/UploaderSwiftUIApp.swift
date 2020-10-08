3ab24cf2ae222b6aeb15f8b0bb2c95d48af8cd81
Steven Rockarts <steven.rockarts@punchcard.io>
2020-10-08 10:54:41 -0600

    Create SwiftUI Project

---
+++ SwiftUI/UploaderSwiftUI/UploaderSwiftUI/UploaderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UploaderSwiftUIApp.swift
+//  UploaderSwiftUI
+//
+//  Created by Steven Rockarts on 2020-10-08.
+//
+
+import SwiftUI
+
+@main
+struct UploaderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
